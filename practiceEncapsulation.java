81c9f8d425c698a10adf94da02cb6849789cb60a
Rods09 <johnrodeldolinog@gmail.com>
2024-08-17 15:14:53 +0800

    practiceEncapsulation.java

---
+++ practiceEncapsulation.java
@@ -0,0 +1,21 @@
+public class Encapsulation1 {
+     String firstName;
+     String lastName;
+
+    // Getter and Setter methods for firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Getter and Setter methods for lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
