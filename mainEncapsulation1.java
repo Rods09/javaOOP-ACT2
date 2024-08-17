58f622f1ce34b2b9feb87eac9119550568c0b53d
Rods09 <johnrodeldolinog@gmail.com>
2024-08-17 15:11:59 +0800

    mainEncapsulation1.java

---
+++ mainEncapsulation1.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+        Encapsulation1 person = new Encapsulation1();
+
+        person.setFirstName("John Rodel");
+        person.setLastName("Dolinog");
+
+        System.out.println("Full Name: " + person.getFirstName() + " " + person.getLastName());
+    }
+}
